7eb7ad2ff688b1a7d8b14c9d1c09a76c372ffb9f
evandrotavaresdeva <evandrotavaredeva@gmail.com>
2023-07-13 19:37:03 -0300

    controllerInfo

---
+++ MinhasVendas.App/Controllers/InfoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhasVendas.App.Controllers
+{
+    public class InfoController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public InfoController(IConfiguration configuration)
+        {
+                _configuration = configuration;
+        }
+        public string Index()
+        {
+            
+
+            var nome = _configuration["Info:Nome"];
+            var conexao = _configuration.GetConnectionString("DefaultConnection");
+
+
+            return $"Info: {nome} / {conexao}";
+        }
+    }
+}
